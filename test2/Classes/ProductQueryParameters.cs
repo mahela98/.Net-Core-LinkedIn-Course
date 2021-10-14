a231b24a51812b106350bc02a53fe66c51565c6d
mahela98 <chandimaofficial@gmail.com>
2021-10-14 07:59:09 +0530

    added filtering

---
+++ test2/Classes/ProductQueryParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test2.Classes
+{
+    public class ProductQueryParameters :QueryParameters
+    {
+        public string Sku { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+
+
+
+
+    }
+}
